aab6cb97a4422b08bf3b6641a6ea694161435712
Joshua Nyoni <joshnyoni@icloud.com>
2026-09-25 18:51:41 +0200

    Exclude test files from Next.js build and add module stubs for Anchor IDL/types

---
+++ src/types/anchor.d.ts
@@ -0,0 +1,8 @@
+declare module '*/target/types/tati_energy_rewards' {
+  export type TatiEnergyRewards = any;
+}
+
+declare module '*/target/idl/tati_energy_rewards.json' {
+  const value: any;
+  export default value;
+}
