2970e3904f4d889f258c568670a071e5911280a0
Joshua Nyoni <joshnyoni@icloud.com>
2026-09-25 19:31:11 +0200

    fix: update root next.config.js and package.json to disable build typechecks

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Allows production builds to successfully complete even if your project has type errors
+    // Allows production builds to successfully complete even if the project has type errors in non-web scripts
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Disables ESLint checking during builds
+    // Disables ESLint checks during Vercel builds
     ignoreDuringBuilds: true,
   },
 };
